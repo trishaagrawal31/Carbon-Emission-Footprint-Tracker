b3023039ee9ab440406faece8cfa8f8ced15581a
trisha <ta1826@g.rit.edu>
2026-02-25 09:27:58 +0400

    renaming

--- GreenPrintCLI.java
+++ GreenPrintCLI.java
@@ -1,73 +1,123 @@
-public class GreenPrintCLI {
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
 /**
-        Entry point for the GreenPrint Carbon Footprint Tracking System.
+ * Interactive CLI for the GreenPrint carbon footprint tracking system.
+ * 
+ * This class provides a menu-driven interface that allows users to log their carbon emissions
+ * across three categories: Transportation, Food, and Energy. 
+ * @author Aaliya
+ */
+public class GreenPrintCLI {
+        /**
+         * The method asks user for sourceID, Date and username, and gives the list of emissions for the user to choose from,
+         * the method then uses FootprintTracker.addEntry() and delivers an output.
+         * @param args
+         */
+        public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        FootprintTracker ft=new FootprintTracker("RIT GreenPrint 2026");
+        
+        System.out.println("Welcome!");
 
-        This class demonstrates how the FootprintTracker system works by:
+        while (true) {
+                try {
+                System.out.println("\n--- Main Menu ---");
+                System.out.println("1. Add Transportation Emission");
+                System.out.println("2. Add Food Emission");
+                System.out.println("3. Add Energy Emission");
+                System.out.println("4. Generate Report & Finish");
+                System.out.print("Choose an option: ");
+                
+                int choice=scanner.nextInt();
+                scanner.nextLine();
 
-        Creating a tracker instance
-        Adding different emission entries (Transportation, Food, Energy)
-        Using polymorphism to manage various emission types
-        Printing a formatted daily emissions summary
+                
+                if (choice==4){
+                        break;
+                }
+                if(choice<1 || choice >3) {  
+                        System.out.println("Invalid choice! Please select 1-3.");
+                        continue;
+                }
 
-        The main method shows multiple users adding different emission
-        records for a single day.
+                
+                System.out.print("Enter Source ID:");
+                String id = scanner.nextLine();
+                if(id.isEmpty()) { System.out.println("ID cannot be empty!"); continue; }
+                System.out.print("Enter Date:");
+                String date = scanner.nextLine();
+                if(date.isEmpty()) { System.out.println("Date cannot be empty!"); continue; }
+                System.out.print("Enter User Name:");
+                String user = scanner.nextLine();
+                if(user.isEmpty()) { System.out.println("User name cannot be empty!"); continue; }
 
-        @author Adira
-**/
+                // ---TRANSPORTATION ---
+                if (choice==1){
+                        System.out.print("Transport Type (Car, Bus, Train, Cycle): ");
+                        String transport = scanner.nextLine().toLowerCase();
 
-    public static void main(String[] args) {
+                        if (transport.equals("car") || transport.equals("bus") || transport.equals("train") || transport.equals("cycle")){
+                        System.out.print("Enter Distance (km): ");
+                        double dist = scanner.nextDouble();
+                        scanner.nextLine();
+                        if (dist<0){
+                                System.out.println("Distance cant be negative. Try again!");
+                        }else{
+                                ft.addEntry(new TransportationEmission(id, "transportation", date, user, dist, transport));
+                                System.out.println("Transportation added.");
+                        }
+                } else{
+                        System.out.println("Error: '" + transport +". Please put something from our system.");
+                }
 
-        FootprintTracker ft = new FootprintTracker("RIT GreenPrint 2026");
+                // --- FOOD ---
+                } else if(choice==2){
+                        System.out.print("Enter Meal Type (Vegan, Vegetarian, Poultry, Beef): ");
+                        String meal = scanner.nextLine().toLowerCase();
 
-        // Explicitly pass category along with other fields
-        ft.addEntry(new TransportationEmission(
-                "T-001",
-                "Transportation",
-                "2026-02-12",
-                "Alice",
-                15,
-                "Car"));
+                        if(meal.equals("vegan") || meal.equals("vegetarian") || meal.equals("poultry") || meal.equals("beef")) {
+                        System.out.print("enter Number of Meals: ");
+                        int meals = scanner.nextInt();
+                        scanner.nextLine();
+                        if(meals<0){
+                                System.out.println("No of meals cant be negative. Try again!");
+                        } else{
+                                ft.addEntry(new FoodEmission(id, "Food", date, user, meal, meals));
+                                System.out.println("Food added.");
+                        }
+                } else{
+                        System.out.println("Error: '" + meal + ". Please put something from our system.");
+                }
 
-        ft.addEntry(new FoodEmission(
-                "F-001",
-                "Food",
-                "2026-02-12",
-                "Alice",
-                "Vegan",
-                2));
+                // --- ENERGY ---
+                } else if(choice == 3){
+                        System.out.print("Enter Energy Source (Grid, Solar, Wind, Coal, Natural Gas, Nuclear, Diesel, Hydro): ");
+                        String energy = scanner.nextLine().toLowerCase();
 
-        ft.addEntry(new EnergyEmission(
-                "E-001",
-                "Energy",
-                "2026-02-12",
-                "alice",
-                8.5,
-                "Grid"));
+                        if(energy.equals("grid") || energy.equals("solar") || energy.equals("wind") || energy.equals("coal") || energy.equals("natural gas") || energy.equals("nuclear") || energy.equals("diesel") || energy.equals("hydro")) {
+                        
+                        System.out.print("Enter kWh used: ");
+                        double kwh = scanner.nextDouble();
+                        scanner.nextLine();
+                        if(kwh<0) {
+                                System.out.println("Energy usage cant be negative. Try again!");
+                        }else{
+                                ft.addEntry(new EnergyEmission(id, "Energy", date, user, kwh, energy));
+                                System.out.println("Energy added.");
+                        }
+                }else{
+                        System.out.println("Error: '" + energy + "'. Please put something in our system.");
+                }
+                }
 
-        ft.addEntry(new TransportationEmission(
-                "T-002",
-                "Transportation",
-                "2026-02-12",
-                "Bob",
-                22,
-                "Bus"));
+                }catch(InputMismatchException e) {
+                System.out.println("ENTER A NUMBER.");
+                scanner.nextLine();
+        }
+        }
 
-        ft.addEntry(new FoodEmission(
-                "F-002",
-                "Food",
-                "2026-02-12",
-                "Charlie",
-                "Beef",1));
-
-        ft.addEntry(new EnergyEmission(
-                "E-002",
-                "Energy",
-                "2026-02-12",
-                "Charlie",
-                5,
-                "Solar"));
-
-        // Generates the final report
         ft.generateDailyReport();
-    }
+        scanner.close();
+}
 }
---
+++ GreenPrintCLI2.java
@@ -0,0 +1,73 @@
+public class GreenPrintCLI2 {
+/**
+        Entry point for the GreenPrint Carbon Footprint Tracking System.
+
+        This class demonstrates how the FootprintTracker system works by:
+
+        Creating a tracker instance
+        Adding different emission entries (Transportation, Food, Energy)
+        Using polymorphism to manage various emission types
+        Printing a formatted daily emissions summary
+
+        The main method shows multiple users adding different emission
+        records for a single day.
+
+        @author Adira
+**/
+
+    public static void main(String[] args) {
+
+        FootprintTracker ft = new FootprintTracker("RIT GreenPrint 2026");
+
+        // Explicitly pass category along with other fields
+        ft.addEntry(new TransportationEmission(
+                "T-001",
+                "Transportation",
+                "2026-02-12",
+                "Alice",
+                15,
+                "Car"));
+
+        ft.addEntry(new FoodEmission(
+                "F-001",
+                "Food",
+                "2026-02-12",
+                "Alice",
+                "Vegan",
+                2));
+
+        ft.addEntry(new EnergyEmission(
+                "E-001",
+                "Energy",
+                "2026-02-12",
+                "alice",
+                8.5,
+                "Grid"));
+
+        ft.addEntry(new TransportationEmission(
+                "T-002",
+                "Transportation",
+                "2026-02-12",
+                "Bob",
+                22,
+                "Bus"));
+
+        ft.addEntry(new FoodEmission(
+                "F-002",
+                "Food",
+                "2026-02-12",
+                "Charlie",
+                "Beef",1));
+
+        ft.addEntry(new EnergyEmission(
+                "E-002",
+                "Energy",
+                "2026-02-12",
+                "Charlie",
+                5,
+                "Solar"));
+
+        // Generates the final report
+        ft.generateDailyReport();
+    }
+}
